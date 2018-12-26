396aa017227d1f4769a80f778f43f34388cf755b
ttakuru88 <asaumi@kray.jp>
2018-12-26 14:50:16 +0900

    時間別が集計できなくなっているのを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   belongs_to :summary
 
-  serialize :message_count_per_hour, Array
+  serialize :messages_count_per_hour, Array
   serialize :count_per_pickup_word, Hash
 
   delegate :pickup_words, to: :summary
