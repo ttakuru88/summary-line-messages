de2b6f994bff6c3f668c7331714351a1a3306904
ttakuru88 <asaumi@kray.jp>
2018-12-26 09:44:47 +0900

    ユーザは時間毎の数をカウントできる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   belongs_to :summary
+
+  def message_count_per_hour=(array)
+    super(array.join(','))
+  end
+
+  def message_count_per_hour
+    super.to_s.split(',').map(&:to_i)
+  end
 end
