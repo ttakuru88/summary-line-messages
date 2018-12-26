b135f876972dafc466ec4cc31b9828cbd14a17a0
ttakuru88 <asaumi@kray.jp>
2018-12-26 10:31:08 +0900

    集計できる

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,10 +1,27 @@
 class SummariesController < ApplicationController
   def new
+    @summary = Summary.new
   end
 
   def create
+    error('ファイルを選択してください') and return if params[:line_messages].blank?
+
+    Summary.transaction do
+      @summary = Summary.create!(name: params[:line_messages].original_filename)
+      @summary.import_from!(params[:line_messages])
+    end
+
+    redirect_to summary_path(id: @summary.uuid)
   end
 
   def show
+  end
+
+  private
+
+  def error(error_message)
+    @summary ||= Summary.new
+    flash.now[:alert] = error_message
+    render :new
   end
 end
--- app/models/summary.rb
+++ app/models/summary.rb
@@ -3,6 +3,43 @@ class Summary < ApplicationRecord
 
   before_create :generate_uuid
 
+  DAY_FORMAT = /^\d\d\d\d\/\d\d\/\d\d/.freeze
+  TIME_FORMAT = /^\d\d:\d\d/.freeze
+
+  def import_from!(uploaded_file)
+    new_users = {}
+    File.open(uploaded_file.path, 'r').read.split("\n").each do |line|
+      if DAY_FORMAT === line
+        self.start_at ||= Time.parse(line)
+        self.end_at = Time.parse(line)
+        next
+      end
+      next unless TIME_FORMAT === line
+
+      date, name, text = line.split("\t").map(&:chomp)
+      next unless text
+
+      new_users[name] ||= {messages_count: 0, kusas_count: 0, photos_count: 0, stamps_count: 0, message_count_per_hour: Array.new(24) { 0 }}
+
+      new_users[name][:messages_count] += 1
+      new_users[name][:stamps_count] += 1 if text == '[スタンプ]'
+      new_users[name][:photos_count] += 1 if text == '[写真]'
+
+      kusa = text.match(/w+$/)
+      new_users[name][:kusas_count] += kusa[0].size if kusa
+
+      h, m = date.split(':').map(&:to_i)
+      new_users[name][:message_count_per_hour][h] += 1
+    end
+
+    importable_new_users = new_users.map do |name, data|
+      data.merge({summary_id: id, name: name, message_count_per_hour: data[:message_count_per_hour].join(',')})
+    end
+    User.import(importable_new_users)
+
+    save!
+  end
+
   private
 
   def generate_uuid
