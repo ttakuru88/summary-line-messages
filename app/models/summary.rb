1b641dcc1bc0c19f7c2cf33a907a23ff9c09726a
ttakuru88 <asaumi@kray.jp>
2018-12-26 09:44:35 +0900

    サマリはuuidを持つ

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,3 +1,11 @@
 class Summary < ApplicationRecord
   has_many :users, dependent: :delete_all
+
+  before_create :generate_uuid
+
+  private
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid if uuid.blank?
+  end
 end
